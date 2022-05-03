53e1e793f76eed171769353e1d4c37749185804e
Kleber Magno <klebermagno@gmail.com>
2022-05-03 18:50:07 -0300

    Fix Queue and Stack implementations and add some tests.

--- src/main/java/com/klebermagno/code/cap3/Queue.java
+++ src/main/java/com/klebermagno/code/cap3/Queue.java
@@ -1,4 +1,55 @@
 package com.klebermagno.code.cap3;
 
-public class Queue {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue<T> {
+
+    private List<T> elements = new ArrayList<>();
+
+    //enqueue
+    public void enqueue(T element){
+        elements.add(element);
+    }
+
+    public T dequeue(){
+        if(elements==null || elements.isEmpty()) throw new RuntimeException("No more elements!");
+        return elements.remove(0);
+    }
+
+    public T peek(){
+        if(elements==null || elements.isEmpty()) throw new RuntimeException("No more elements!");
+        return elements.get(0);
+    }
+
+    public boolean isEmpty(){
+        return elements.isEmpty();
+    }
+
+    public int count(){
+        return elements.size();
+    }
+
+    public void show(){
+        elements.stream().forEach(System.out::println);
+    }
+
+    public static void main(String[] args){
+        Queue<String> queue = new Queue();
+        queue.enqueue("First");
+        queue.enqueue("Second");
+        queue.enqueue("Third");
+        queue.show();
+        String s = queue.dequeue();
+        System.out.println(s);
+        assertEquals("First",s);
+    }
+
+    public static void assertEquals(Object expected, Object received){
+        if(expected.equals(received)){
+            System.out.println("Success!");
+        }else{
+            System.out.println("Fail!");
+        }
+    }
 }
--- src/main/java/com/klebermagno/code/cap3/Stack.java
+++ src/main/java/com/klebermagno/code/cap3/Stack.java
@@ -12,7 +12,7 @@ public class Stack<T> {
      * O(1)
      * @param element
      */
-    public void pop(T element){
+    public void push(T element){
         elements.add(element);
     }
 
@@ -20,7 +20,7 @@ public class Stack<T> {
      * O(n)
      * @return
      */
-    public T push(){
+    public T pop(){
         if (elements== null || elements.isEmpty()) throw new EmptyStackException();
         return elements.remove(elements.size()-1);
     }
@@ -32,5 +32,29 @@ public class Stack<T> {
     public T peek(){
         if (elements== null || elements.isEmpty()) throw new EmptyStackException();
         return elements.get(elements.size()-1);
+    }
+
+    public static void main(String[] args){
+        Stack<String> stack = new Stack();
+        stack.push("First");
+        stack.push("Second");
+        stack.push("Third");
+        stack.show();
+        String s = stack.pop();
+        System.out.println(s);
+        assertEquals("Third",s);
+        stack.show();
+    }
+
+    private void show() {
+        elements.stream().forEach(System.out::println);
+    }
+
+    public static void assertEquals(Object expected, Object received){
+        if(expected.equals(received)){
+            System.out.println("Success!");
+        }else{
+            System.out.println("Fail!");
+        }
     }
 }
